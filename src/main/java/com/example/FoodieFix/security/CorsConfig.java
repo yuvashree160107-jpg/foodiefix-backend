2f0c76942a921c4cecd02a0456a6f0f1082e25b3
yuvashree160107-jpg <yuvashree160107@gmail.com>
2026-07-08 11:21:27 +0530

    Update CorsConfig.java

--- src/main/java/com/example/FoodieFix/security/CorsConfig.java
+++ src/main/java/com/example/FoodieFix/security/CorsConfig.java
@@ -14,8 +14,11 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5500","http://127.0.0.1:5500","http://localhost:5501",
-            "http://127.0.0.1:5501","http://127.0.0.1:5502/","https://yuvashree160107.github.io/"
+        config.setAllowedOrigins(List.of(
+            "http://localhost:5500","http://127.0.0.1:5500",
+            "http://localhost:5501","http://127.0.0.1:5501",
+            "http://127.0.0.1:5502",
+            "https://yuvashree160107.jpg.github.io"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
